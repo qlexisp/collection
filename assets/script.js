50b1b8eb8e4b1d53bfad24452260094ab5ee28bc
Alexis P <1alexispetropoulos@gmail.com>
2023-12-14 12:26:02 +0100

    avancée project collection

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ const collection = [
         year: "1997",
         location: "Memphis",
         tracks: ["Motivated", "Late Nite Tip"],
+        cover: "assets/images/world_domination.jpg",
     },
     {
         title: "Illmatic",
@@ -12,6 +13,7 @@ const collection = [
         year: "1994",
         location: "New York",
         tracks: ["The World is Yours", "Life's a Bitch"],
+        cover: "assets/images/illmatic.jpg",
     },
     {
         title: "Moment of Truth",
@@ -19,6 +21,7 @@ const collection = [
         year: "1998",
         location: "New York",
         tracks: ["Moment of Truth", "Betrayal"],
+        cover: "assets/images/moment_of_truth.jpg",
     },
     {
         title: "The War Report",
@@ -26,6 +29,7 @@ const collection = [
         year: "1997",
         location: "New York",
         tracks: ["Illegal Life", "Black Gangstas"],
+        cover: "assets/images/the_war_report.png",
     },
     {
         title: "The Low End Theory",
@@ -33,6 +37,7 @@ const collection = [
         year: "1991",
         location: "New York",
         tracks: ["Buggin' Out", "Check the Rhime"],
+        cover: "assets/images/low_end_theory.jpg",
     },
     {
         title: "My Balls and my World",
@@ -40,6 +45,7 @@ const collection = [
         year: "1998",
         location: "Bâton Rouge",
         tracks: ["Pull It Off", "An Offer U can't Refuse"],
+        cover: "assets/images/my_balls_and_my_world.jpg",
     },
     {
         title: "Lifestylez ov Da Poor & Dangerous",
@@ -47,6 +53,7 @@ const collection = [
         year: "1995",
         location: "New York",
         tracks: ["Danger Zone", "Street Struck"],
+        cover: "assets/images/lifestylez_ov_da_poor_and_dangerous.jpg",
     },
     {
         title: "Labcabincalifornia",
@@ -54,6 +61,7 @@ const collection = [
         year: "1995",
         location: "Los Angeles",
         tracks: ["Runnin", "Drop"],
+        cover: "assets/images/labcabincalifornia.jpg",
     },
     {
         title: "93 Til Infinity",
@@ -61,6 +69,7 @@ const collection = [
         year: "1993",
         location: "Los Angeles",
         tracks: ["93 Til Infinity", "Let 'Em Know"],
+        cover: "assets/images/93tilinfinity.jpg",
     },
     {
         title: "Ready to Die",
@@ -68,6 +77,7 @@ const collection = [
         year: "1995",
         location: "New York",
         tracks: ["Things Done Changed", "Ready to Die"],
+        cover: "assets/images/ready_to_die.jpg",
     },
 ];
 // Je crée la collection en faisant en sorte d'avoir cinq propriétés
@@ -78,6 +88,9 @@ conteneur.id = "collection_conteneur"; // Je nomme le div "collection_conteneur"
 for (const item of collection) { // Pour chaque item de la collection..
     const div = document.createElement("div"); // On crée un div
     div.className = "div_items"; // Je nomme les div "div_items" pour plus de clarté (Pas la même chose que "item", qui sont les propriétés de mon array)
+
+    const cover = document.createElement("img");
+    cover.src = item.cover
 
     const titre = document.createElement("h1"); // On crée un h1 pour les titres
     titre.textContent = item.title; 
@@ -92,8 +105,9 @@ for (const item of collection) { // Pour chaque item de la collection..
     location.textContent = `Localisation: ${item.location}`;
 
     const tracks = document.createElement("p"); // Ajout des tracks en tant que liste
-    tracks.textContent = `Morceaux préférés: ${item.tracks}`; // /!\ Trouver un moyen de mettre un espace entre la virgule et le deuxième morceau /!\
+    tracks.textContent = `Morceaux préférés: ${item.tracks.join(", ")}`; // .join(", ") permet de créer un espace entre la virgule et la deuxième donnée
     
+    div.appendChild(cover);
     div.appendChild(titre);
     div.appendChild(artist);
     div.appendChild(year);
