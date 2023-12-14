0c372a759f34c10884524e39825cfcabcdee5402
root <root@DESKTOP-LCHE35F>
2023-12-14 09:09:19 +0100

    14-12

--- assets/script.js
+++ assets/script.js
@@ -70,17 +70,35 @@ const collection = [
         tracks: ["Things Done Changed", "Ready to Die"],
     },
 ];
-
 // Je crée la collection en faisant en sorte d'avoir cinq propriétés
 
 const conteneur = document.createElement("div"); // Création d'un conteneur dans lequel seront tous les items
 conteneur.id = "collection_conteneur"; // Je nomme le div "collection_conteneur" pour plus de clarté
 
-for (const items of collection) { // Pour chaque item de la collection..
+for (const item of collection) { // Pour chaque item de la collection..
     const div = document.createElement("div"); // On crée un div
-    div.className = "div_items"; // Je nomme les div "div_items" pour plus de clarté
-    const contenu = document.createTextNode(`${items.title}, ${items.artist}, ${items.year}, ${items.location}, ${items.tracks}`); // Dans lequel on affiche les propriétés de l'item
-    div.appendChild(contenu);
+    div.className = "div_items"; // Je nomme les div "div_items" pour plus de clarté (Pas la même chose que "item", qui sont les propriétés de mon array)
+
+    const titre = document.createElement("h1"); // On crée un h1 pour les titres
+    titre.textContent = item.title; 
+
+    const artist = document.createElement("p"); // Ajout de l'artiste en tant que paragraphe
+    artist.textContent = `Artiste: ${item.artist}`;
+
+    const year = document.createElement("p"); // Ajout de l'année en tant que paragraphe
+    year.textContent = `Année: ${item.year}`;
+
+    const location = document.createElement("p"); // Ajout de la localisation en tant que paragraphe
+    location.textContent = `Localisation: ${item.location}`;
+
+    const tracks = document.createElement("p"); // Ajout des tracks en tant que liste
+    tracks.textContent = `Morceaux préférés: ${item.tracks}`; // /!\ Trouver un moyen de mettre un espace entre la virgule et le deuxième morceau /!\
+    
+    div.appendChild(titre);
+    div.appendChild(artist);
+    div.appendChild(year);
+    div.appendChild(location);
+    div.appendChild(tracks);
     conteneur.appendChild(div);
 }
 document.body.appendChild(conteneur);
